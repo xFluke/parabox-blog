74a486de4d2960ce97825f26a8ae5dc6a7e50e41
xFluke <mikoman1124@gmail.com>
2026-07-16 00:54:07 -0400

    Modified Astro Header for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://xfluke.github.io/',
+	base: '/parabox-blog/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
